4b82a7a363c1bd4b386e82be4b1d2deaa6f2ba7f
Maaj <maajb1122@gmail.com>
2026-03-12 16:57:41 +0530

    lets start

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-center font-bold text-2xl text-blue-400'>Allah is great</h1>
+    </>
+  )
+}
+
+export default App
